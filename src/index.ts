a13518b0e109b1b0988c06d5d56791b49448170c
Max Nowack <max@unsou.de>
2019-01-14 11:53:49 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import send, { Measurement, defaultEndpoint } from './send'
+export { default as send, Measurement } from './send'
+
+export interface Options {
+  email: string
+  token: string
+  endpoint: string
+  interval: number
+}
+
+export default class Appoptics {
+  private options: Options
+  private measurements: Measurement[] = []
+  private intervalId: NodeJS.Timeout | null = null
+  private started = false
+
+  constructor({
+    email,
+    token,
+    endpoint = defaultEndpoint,
+    interval = 10000,
+  }: Options) {
+    this.options = { email, token, endpoint, interval }
+  }
+
+  public measure(measurement: Measurement) {
+    if (!this.started) this.start()
+    this.measurements.push({
+      time: new Date(),
+      ...measurement,
+    })
+  }
+
+  public start() {
+    this.intervalId = setInterval(() => this.flush(), this.options.interval)
+    this.started = true
+  }
+  public stop() {
+    if (this.intervalId) clearInterval(this.intervalId)
+    this.started = false
+  }
+  public flush() {
+    const measurements = this.measurements
+    this.measurements = []
+    return send({
+      measurements,
+      email: this.options.email,
+      token: this.options.token,
+      endpoint: this.options.endpoint,
+    })
+  }
+}
---
+++ src/send.ts
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch'
+
+export const defaultEndpoint = 'https://api.appoptics.com/v1/measurements'
+
+export interface Tag {
+  [key: string]: string
+}
+export interface Measurement {
+  name: string
+  value: number
+  time?: Date
+  tags?: Tag[]
+  period?: number
+  count?: number
+  sum?: number
+  max?: number
+  min?: number
+  last?: number
+  stddev?: number
+  stddev_m2?: number
+}
+export interface Options {
+  measurements: Measurement[]
+  tags?: Tag[]
+  email: string
+  token: string
+  endpoint: string
+}
+
+export default ({
+  measurements = [],
+  tags = [],
+  email,
+  token,
+  endpoint = defaultEndpoint,
+}: Options) => fetch(endpoint, {
+  method: 'POST',
+  headers: {
+    body: JSON.stringify({
+      ...tags,
+      measurements,
+    }),
+    Authorization: 'Basic ' + new Buffer(`${email}:${token}`).toString('base64'),
+    'Content-Type': 'application/json; charset=utf-8',
+    'User-Agent': 'appoptics-node',
+  },
+}).then(r => r.json())
